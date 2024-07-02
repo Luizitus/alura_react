f2e4f8905417703eb3598c6ed61f17547d51a2bd
Luizitu Moreira <luizitus@gmail.com>
2024-07-02 17:47:14 -0300

    ajuste
    Ajuste

--- organo/src/componentes/Banner/Banner.jsx
+++ organo/src/componentes/Banner/Banner.jsx
@@ -2,10 +2,8 @@ import './Banner.css'
 
 export const Banner = () => {
     return(
-        <div>
-            <header className="banner">
-                <img src="/imagens/banner.png" alt="Banner principal da página Organo"/>
-            </header>
-        </div>
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página Organo"/>
+        </header>
     )
 }
